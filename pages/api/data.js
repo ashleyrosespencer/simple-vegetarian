c1d608ab24af7ab72a42a15d69ce64b24d3e2d7c
ashleyshubin <86271205+ashleyshubin@users.noreply.github.com>
2023-07-03 14:09:55 -0700

    Added recipes page/dynamic routing

---
+++ pages/api/data.js
@@ -0,0 +1,183 @@
+export default {
+    Trending : [
+        {
+            id: 1,
+            title: "Tofu \"Poke\" Bowls",
+            subtitle: "Fast, easy, and delicious",
+            category: "Hawaiian, Bowls",
+            img: "/images/tofu-poke-bowl-trending.png",
+            description: "This Tofu Poke Bowl recipe offers a nutritious, delightful meal for any time of the day. Start by marinating firm tofu in a flavorful blend of oil, tamari, and spices before baking it to a crispy perfection. As the tofu bakes, cook quinoa or rice and prepare fresh toppings such as sliced avocado, green onions, and cucumber. Finally, assemble the bowl by combining the baked tofu, cooked quinoa or rice, and vibrant vegetables for a satisfying and wholesome poke bowl experience that bursts with flavor and texture.",
+            published: "June 20, 2023",
+            author:{
+                name: "",
+                img: "",
+                designation: ""
+            }
+        },
+        {
+            id: 2,
+            title: "Sweet Potato Tostadas",
+            subtitle: "Crispy, fresh, spicy",
+            category: "Mexican",
+            img: "/images/sweet-potato-tostadas-trending.jpg",
+            description: "Crispy tortillas serve as a base for a tantalizing combination of seasoned black beans, zesty and spicy sweet potatoes, creamy avocado slices, crumbled cotija cheese, and a tangy lime-sour cream sauce. With every bite, you'll experience the perfect balance of textures and a harmonious blend of savory, spicy, and creamy notes. These tostadas are a delightful fusion of Mexican-inspired ingredients, offering a satisfying and unforgettable culinary experience.",
+            published: "June 22, 2023",
+            author:{
+                name: "",
+                img: "",
+                designation: ""
+            }
+        },
+        {
+            id: 3,
+            title: "Greek Buddha Bowls",
+            subtitle: "Fresh, tangy, delightful",
+            category: "Greek, Bowls",
+            img: "/images/greek-buddha-bowl-trending.png",
+            description: "Bursting with fresh vegetables, protein-packed garbanzo beans, tangy feta cheese, and a delightful combination of hummus and tzatziki sauce, this bowl is a wholesome and satisfying meal. The marinated garbanzo beans add a zesty kick, while the crisp cucumber, juicy cherry tomatoes, and briny Kalamata olives bring a burst of freshness. Layered over fluffy quinoa and topped with creamy feta cheese, this Greek Buddha Bowl is a nutritious and delicious way to enjoy the vibrant flavors of Greek cuisine in a single bowl.",
+            published: "June 21, 2023",
+            author:{
+                name: "",
+                img: "",
+                designation: ""
+            }
+        }
+    ],
+    Posts : [
+        {
+            id: 1,
+            title: "Tofu \"Poke\" Bowls",
+            subtitle: "This Tofu Poke Bowl keeps it simple and satisfying, highlighting the goodness of fresh vegetables and baked tofu in an easy-to-make recipe that's sure to please.",
+            category: "Hawaiian, Bowls",
+            img: "/images/tofu-poke-bowl.jpg",
+            description: "This Tofu Poke Bowl recipe offers a nutritious, delightful meal for any time of the day. Start by marinating firm tofu in a flavorful blend of oil, tamari, and spices before baking it to a crispy perfection. As the tofu bakes, cook quinoa or rice and prepare fresh toppings such as sliced avocado, green onions, and cucumber. Finally, assemble the bowl by combining the baked tofu, cooked quinoa or rice, and vibrant vegetables for a satisfying and wholesome poke bowl experience that bursts with flavor and texture.",
+            directions: "Preheat your oven to 400°F (200°C) and line a baking sheet with parchment paper.; Drain the tofu and gently press it between paper towels or a clean kitchen towel to remove excess moisture.; Cut the tofu into bite-sized cubes.; In a bowl, whisk together the oil, tamari or soy sauce, garlic salt, paprika, and pepper.; Add the tofu to the bowl and gently toss it until all the pieces are coated with the marinade.; Allow the tofu to marinate for at least 15 minutes, or longer if time permits.; Transfer the marinated tofu to the prepared baking sheet, arranging the pieces in a single layer.; Bake in the preheated oven for 20-25 minutes, or until the tofu turns golden brown and slightly crispy - you can flip the tofu halfway through baking for even browning.; While the tofu is baking, cook the quinoa or rice according to the package instructions.; Slice the avocado, green onions, and cucumber and set them aside.; Once baked, remove the tofu from the oven and let it cool slightly.; To assemble the poke bowls, start with a base of cooked quinoa or rice in two bowls.; Add the baked tofu, sliced avocado, green onions, and cucumber on top between both bowls.; Garnish with sesame seeds or any additional toppings if desired.; Serve the tofu poke bowls immediately and enjoy!;",
+            published: "June 20, 2023",
+            ingredient1: "1 cup quinoa or rice",
+            ingredient2: "14 ounces (1 block) extra firm tofu, drained and cut into 1/2 inch cubes",
+            ingredient3: "1/2 cup edamame, shelled",
+            ingredient4: "1 ripe avocado, cut into slices",
+            ingredient5: "2 tablespoons kimchi (I use Lucky Foods Seoul Original Kimchi)",
+            ingredient6: "1 cup cucumber, sliced into half moons",
+            ingredient7: "",
+            ingredient8: "2 green onions, sliced",
+            ingredient9: "1/2 teaspoon black sesame seeds (optional)",
+            ingredient10: "2 tablespoons tamari",
+            ingredient11: "2 tablespoons sesame oil",
+            ingredient12: "1/2 teaspoon garlic salt",
+            ingredient13: "1/2 teaspoon paprika",
+            ingredient14: "1/2 teaspoon pepper",
+
+
+            author:{
+                name: "Ashley Shubin",
+                img: "/images/author/ashley-shubin.jpg",
+                designation: "Creator of Recipes"
+            }
+        },
+        {
+            id: 2,
+            title: "Sweet Potato Tostadas",
+            subtitle: "Indulge in a fiesta of flavors with these Sweet Potato Tostadas—crispy tortillas topped with seasoned black beans, spicy sweet potatoes, creamy avocado, tangy cotija cheese, and a zesty lime-sour cream sauce, creating a mouthwatering symphony of tastes that will leave you craving more.",
+            category: "Mexican",
+            img: "/images/sweet-potato-tostadas.jpg",
+            description: "Crispy tortillas serve as a base for a tantalizing combination of seasoned black beans, zesty and spicy sweet potatoes, creamy avocado slices, crumbled cotija cheese, and a tangy lime-sour cream sauce. With every bite, you'll experience the perfect balance of textures and a harmonious blend of savory, spicy, and creamy notes. These tostadas are a delightful fusion of Mexican-inspired ingredients, offering a satisfying and unforgettable culinary experience.",
+            directions: "Preheat oven to 400°F (200°C). Place the corn tortillas on a baking sheet and bake for about 5-7 minutes until they become crispy. Remove from the oven and set aside.; In a separate bowl, toss the diced sweet potatoes with paprika, cayenne pepper, garlic salt, and a drizzle of olive oil. Spread them out on a baking sheet and bake for 20-25 minutes until tender and slightly crispy.; In a small bowl, mix the sour cream with lime juice to create a tangy sauce.; To assemble the tostadas, spread a layer of the mashed black beans onto each crispy corn tortilla. Top with a generous amount of the baked sweet potatoes, sliced avocado, and crumbled cotija cheese. Drizzle the sour cream and lime sauce over the top.; Garnish with fresh cilantro leaves and season with additional salt and pepper to taste.;",
+            published: "June 22, 2023",
+            ingredient1: "4 corn tortillas",
+            ingredient2: "2 small sweet potatoes, peeled and diced",
+            ingredient3: "2 cups black beans (cooked)",
+            ingredient4: "1 teaspoon cumin",
+            ingredient5: "1 teaspoon chili powder",
+            ingredient6: "1 teaspoon paprika",
+            ingredient7: "1/2 teaspoon garlic salt",
+            ingredient8: "1 ripe avocado, sliced",
+            ingredient9: "1/4 crumbled cotija cheese",
+            ingredient10: "1/4 cup sour cream",
+            ingredient12: "Juice of 1 lime",
+            ingredient13: "Fresh cilantro leaves for garnish",
+            ingredient14: "Salt and pepper to taste",
+
+
+            author:{
+                name: "Ashley Shubin",
+                img: "/images/author/ashley-shubin.jpg",
+                designation: "Creator of Recipes"
+            }
+        },
+        {
+            id: 3,
+            title: "Greek Buddha Bowls",
+            subtitle: "Embrace Mediterranean goodness in a bowl! This Greek Buddha Bowl combines vibrant vegetables, marinated garbanzo beans, and creamy hummus to create a delightful and nourishing meal that celebrates the flavors of Greece.",
+            category: "Greek, Bowls",
+            img: "/images/greek-buddha-bowl.jpg",
+            description: "Bursting with fresh vegetables, protein-packed garbanzo beans, tangy feta cheese, and a delightful combination of hummus and tzatziki sauce, this bowl is a wholesome and satisfying meal. The marinated garbanzo beans add a zesty kick, while the crisp cucumber, juicy cherry tomatoes, and briny Kalamata olives bring a burst of freshness. Layered over fluffy quinoa and topped with creamy feta cheese, this Greek Buddha Bowl is a nutritious and delicious way to enjoy the vibrant flavors of Greek cuisine in a single bowl.",
+            directions: "In a bowl, combine garbanzo beans and red onion slices. Drizzle with balsamic vinegar and a pinch of salt. Allow them to marinate for 10-15 minutes.; While the garbanzo beans marinate, cook quinoa according to package instructions. Set aside once cooked.; In a separate bowl, prepare the hummus sauce by combining hummus, balsamic vinegar, lemon juice, olive oil, salt, and pepper. Stir the ingredients together until well combined. If the sauce is too thick, add water, a tablespoon at a time, until desired consistency is reached.; Set the hummus sauce aside.; Prepare the vegetables by slicing the cucumber and halving the cherry tomatoes.; In a serving bowl, layer cooked quinoa as the base.; Arrange the marinated garbanzo beans with red onions, diced cucumber, cherry tomatoes, and Kalamata olives on top of the quinoa.; Sprinkle crumbled feta cheese over the vegetables.; Drizzle the prepared hummus sauce and dollop tzatziki sauce generously.; Serve the Greek Buddha Bowls immediately and enjoy!",
+            published: "June 21, 2023",
+            ingredient1: "1 cup quinoa",
+            ingredient2: "1 can (15 oz) garbanzo beans, drained and rinsed",
+            ingredient3: "1/2 red onion, diced",
+            ingredient4: "1 cucumber, sliced into half moons",
+            ingredient5: "3/4 cup cherry tomatoes, halved",
+            ingredient6: "1/2 cup feta cheese, crumbled",
+            ingredient7: "1/2 cup hummus",
+            ingredient8: "2 tablespoons balsamic vinegar + additional for drizzling",
+            ingredient9: "1 tablespoon lemon juice",
+            ingredient10: "1 tablespoon olive oil",
+            ingredient12: "1/2 teaspoon garlic salt",
+            ingredient13: "1/2 teaspoon paprika",
+            ingredient14: "1/2 teaspoon pepper",
+
+
+            author:{
+                name: "Ashley Shubin",
+                img: "/images/author/ashley-shubin.jpg",
+                designation: "Creator of Recipes"
+            }
+        },
+    ],
+    Popular : [
+        {
+            id: 1,
+            title: "Tofu \"Poke\" Bowls",
+            subtitle: "Fast, easy, and delicious",
+            category: "Hawaiian, Bowls",
+            img: "/images/tofu-poke-bowl.jpg",
+            description: "This Tofu Poke Bowl recipe offers a nutritious, delightful meal for any time of the day. Start by marinating firm tofu in a flavorful blend of oil, tamari, and spices before baking it to a crispy perfection. As the tofu bakes, cook quinoa or rice and prepare fresh toppings such as sliced avocado, green onions, and cucumber. Finally, assemble the bowl by combining the baked tofu, cooked quinoa or rice, and vibrant vegetables for a satisfying and wholesome poke bowl experience that bursts with flavor and texture.",
+            published: "June 20, 2023",
+            author:{
+                name: "",
+                img: "",
+                designation: ""
+            }
+        },
+        {
+            id: 2,
+            title: "Sweet Potato Tostadas",
+            subtitle: "Crispy, fresh, spicy",
+            category: "Mexican",
+            img: "/images/sweet-potato-tostadas.jpg",
+            description: "Crispy tortillas serve as a base for a tantalizing combination of seasoned black beans, zesty and spicy sweet potatoes, creamy avocado slices, crumbled cotija cheese, and a tangy lime-sour cream sauce. With every bite, you'll experience the perfect balance of textures and a harmonious blend of savory, spicy, and creamy notes. These tostadas are a delightful fusion of Mexican-inspired ingredients, offering a satisfying and unforgettable culinary experience.",
+            published: "June 22, 2023",
+            author:{
+                name: "",
+                img: "",
+                designation: ""
+            }
+        },
+        {
+            id: 3,
+            title: "Sweet Potato Tostadas",
+            subtitle: "Crispy, fresh, spicy",
+            category: "Mexican",
+            img: "/images/sweet-potato-tostadas.jpg",
+            description: "Crispy tortillas serve as a base for a tantalizing combination of seasoned black beans, zesty and spicy sweet potatoes, creamy avocado slices, crumbled cotija cheese, and a tangy lime-sour cream sauce. With every bite, you'll experience the perfect balance of textures and a harmonious blend of savory, spicy, and creamy notes. These tostadas are a delightful fusion of Mexican-inspired ingredients, offering a satisfying and unforgettable culinary experience.",
+            published: "June 22, 2023",
+            author:{
+                name: "",
+                img: "",
+                designation: ""
+            }
+        }
+    ]
+}
---
+++ pages/recipes/[id].js
@@ -0,0 +1,63 @@
+import { useRouter } from 'next/router';
+import data from '../api/data';
+import Format from '../../layout/format';
+import Image from 'next/image';
+
+export default function Recipe() {
+    const router = useRouter();
+    const { id } = router.query;
+
+    // Find the recipe based on the ID
+    const recipe = data.Posts.find((recipe) => recipe.id === Number(id));
+
+    if (!recipe) {
+        return <div>Recipe not found</div>;
+    }
+
+    const ingredients = [];
+    for (let i = 1; i <= 14; i++) {
+        const ingredientKey = `ingredient${i}`;
+        if (recipe[ingredientKey]) {
+            ingredients.push(recipe[ingredientKey]);
+        }
+    }
+
+    return (
+        <Format>
+            <div>
+                <section className="container mx-auto px-4 md:px-8 py-8 flex flex-wrap">
+                    <div className="w-full md:w-3/4 md:pr-8">
+                        <div className="post py-10">
+                            <h1 className="font-bold text-4xl text-center pb-5 -mt-8">
+                                {recipe.title}
+                            </h1>
+                            <p className="text-gray-500 text-lg text-center">
+                                {recipe.subtitle}
+                            </p>
+                            <div className="py-10 justify-center">
+                                <div className="flex justify-center"></div>
+                                <Image src={recipe.img} width={900} height={600} alt=""/>
+                            </div>
+                            <div className="content text-gray-600 text-lg flex-col gap-4 pb-10 border-b-2 border-gray-500">
+                                <p>{recipe.description}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-full md:w-1/4 mt-8 md:mt-0 px-2">
+                        <div className="pb-5 py-12">
+                            <h1 className="font-bold text-2xl pb-2 border-b-2 border-gray-500 -mt-8">
+                                Ingredients
+                            </h1>
+                            <p className="pt-2 text-md">Servings: 2</p>
+                            <ul className="mt-5 list-disc">
+                                {ingredients.map((ingredient, index) => (
+                                    <li key={index} className="ml-4">{ingredient}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </Format>
+    );
+}
